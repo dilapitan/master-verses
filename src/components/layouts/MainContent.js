8575f4f2bc74b5c5ea206278be38ff1e0fd902a1
dilapitan <domlapitan@gmail.com>
2020-07-28 19:22:00 +0800

    Retrieve data from firestore and pass to views components

--- src/components/layouts/MainContent.js
+++ src/components/layouts/MainContent.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import { Switch, Route } from 'react-router-dom'
 
@@ -6,12 +6,36 @@ import About from '../views/About'
 import MemoryVersesList from '../views/MemoryVersesList'
 import Randomize from '../views/Randomize'
 
+import { getMemoryVerses } from '../../services/FirestoreService'
+
 function MainContent() {
+  const [verses, setVerses] = useState([])
+
+  useEffect(() => {
+    let mv = []
+    getMemoryVerses()
+      .then((querySnapshot) => {
+        querySnapshot.docs.forEach((doc) => {
+          mv.push(doc.data())
+        })
+        setVerses(mv)
+      })
+      .catch((error) => console.log(error))
+  }, [])
+
   return (
     <div>
       <Switch>
-        <Route path="/" exact component={MemoryVersesList} />
-        <Route path="/randomize" exact component={Randomize} />
+        <Route
+          path="/"
+          exact
+          render={() => <MemoryVersesList verses={verses} />}
+        />
+        <Route
+          path="/randomize"
+          exact
+          render={() => <Randomize verses={verses} />}
+        />
         <Route path="/about" exact component={About} />
       </Switch>
     </div>
--- src/components/views/MemoryVersesList.js
+++ src/components/views/MemoryVersesList.js
@@ -1,7 +1,16 @@
 import React from 'react'
 
-function MemoryVersesList() {
-  return <div>Memory Verses List</div>
+import { Divider, Typography } from '@material-ui/core'
+
+function MemoryVersesList({ verses }) {
+  if (verses.length) console.log(verses)
+
+  return (
+    <div>
+      <Typography variant="h6">Memory Verses List</Typography>
+      <Divider />
+    </div>
+  )
 }
 
 export default MemoryVersesList
